64f051ed1b944c8ec55f6b4f80b52f0970ef3130
sachhyam <tsachhyam@gmail.com>
2020-03-16 23:27:39 +0545

    Fixed key issues

--- src/components/ChessBoard/ChessPieces/ChessPieces.js
+++ src/components/ChessBoard/ChessPieces/ChessPieces.js
@@ -3,14 +3,14 @@ import React from 'react'
 import ChessPiece from './ChessPiece/ChessPiece'
 
 const ChessPieces = props => {
-    const pieces = props.position.map((row)=>(
-        <div>
+    const pieces = props.position.map((row,index1)=>(
+        <div key={index1}>
             {
-                row.map((piece)=>{
-                    console.log(toString(piece))
+                row.map((piece,index2)=>{
                     return (
-                    <ChessPiece pieceId={piece}/>
-                )})
+                        <ChessPiece pieceId={piece} key={ piece===0 ? (index1.toString()+ index2.toString()) : piece}/>
+                    )
+                })
             }
         </div>
        
